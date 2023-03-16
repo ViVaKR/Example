9f07b7731e8cad47750d2f98d92fddffab71c4c3
ViVaKR <admin@vivabj.com>
2023-03-16 17:38:09 +0900

    Add my_module [Div] & Comment

--- my_module.py
+++ my_module.py
@@ -11,6 +11,6 @@ def mul(a, b):
 
 
 def div(a, b):
-    return a / b
+    return a / b # 주석을 추가함, 토픽 브랜치와 충돌 발생용 
 
-print(f"123 + 4546 = {add(123, 4546)}")
+print(f"123 + 4546 = {add(123, 4546)}") 
