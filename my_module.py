13e9d62e585948e10617ed0560931f7719b01198
ViVaKR <admin@vivabj.com>
2023-03-16 17:15:00 +0900

    modified my_module [print]

--- my_module.py
+++ my_module.py
@@ -5,8 +5,12 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+
 def mul(a, b):
     return a * b
 
-def div(a,b):
+
+def div(a, b):
     return a / b
+
+print(f"123 + 4546 = {add(123, 4546)}")
